f5161608a7e264a68e4707199a42fa4a12c9581c
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-18 14:01:27 -0300

    feat: make search tags functional

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,8 +2,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { SearchBoxStyle } from "./styles/SearchBoxStyle";
 import TagSearchValue from "./TagSearchValue";
+import { useRef } from "react";
 
 const SearchBox = () => {
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  const handleSearch = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    if (searchInput.current != null) {
+      console.log(searchInput.current.value);
+    }
+  };
+
+  const handleTag = (tag: string) => {
+    if (searchInput.current != null) {
+      searchInput.current.value = tag;
+    }
+  };
+
   return (
     <SearchBoxStyle>
       <div className="flex gap-3 flex-col sm:flex-row">
@@ -11,17 +28,28 @@ const SearchBox = () => {
           type="text"
           id="searchBox"
           placeholder="Search for something cool!"
+          ref={searchInput}
         />
-        <button id="searchButton">
+        <button id="searchButton" onClick={handleSearch}>
           <FontAwesomeIcon icon={faMagnifyingGlass} /> Search
         </button>
       </div>
       <div className="flex gap-2 justify-center flex-wrap">
-        <TagSearchValue text="Movie" iconName="faVideo"></TagSearchValue>
-        <TagSearchValue text="Pepper" iconName="faPepperHot"></TagSearchValue>
-        <TagSearchValue text="Star" iconName="faStar"></TagSearchValue>
-        <TagSearchValue text="Nature" iconName="faLeaf"></TagSearchValue>
-        <TagSearchValue text="Moon" iconName="faMoon"></TagSearchValue>
+        <div onClick={() => handleTag("Pepper")}>
+          <TagSearchValue text="Pepper" iconName="faPepperHot"></TagSearchValue>
+        </div>
+
+        <div onClick={() => handleTag("Star")}>
+          <TagSearchValue text="Star" iconName="faStar"></TagSearchValue>
+        </div>
+
+        <div onClick={() => handleTag("Leaf")}>
+          <TagSearchValue text="Nature" iconName="faLeaf"></TagSearchValue>
+        </div>
+
+        <div onClick={() => handleTag("Moon")}>
+          <TagSearchValue text="Moon" iconName="faMoon"></TagSearchValue>
+        </div>
       </div>
     </SearchBoxStyle>
   );
